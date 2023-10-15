edcb1288bf61d76328a9639e4880ea3d898ef1f9
h602112 <markusnedrevold@live.no>
2023-10-15 12:27:48 -0700

    Working on fully integrating yt functionality

--- axlearner_app.py
+++ axlearner_app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask import render_template
+from search import youtube_search
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', youtube_search=youtube_search)
     
 @app.route('/about')
 def about():
--- search.py
+++ search.py
@@ -3,7 +3,8 @@ import argparse
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-DEVELOPER_KEY = 'AIzaSyCPMNP0LockGe17gTO_ycArVA8iLfJU3Ck'
+f = open('/home/rocotics/WebDev/yt_key', 'r')
+DEVELOPER_KEY = f.read()
 YOUTUBE_API_SERVICE_NAME = 'youtube'
 YOUTUBE_API_VERSION = 'v3'
 
@@ -14,9 +15,36 @@ def youtube_search(options):
         q=options.q,
         part='id,snippet',
         maxResults=options.max_results
-    ).execute()
+        ).execute() 
     
     videos = []
     channels = []
     playlists = []
+    
+    for search_result in search_respone.get('items', []):
+        if search_result['id']['kind'] == 'youtube#video':
+            videos.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['videoId']))
+            
+        elif search_result['id']['kind'] == 'youtube#channel':
+            channels.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['channelId']))
+            
+        elif search_result['id']['kind'] == 'youtube#playlist':
+            playlists.append('%s (%s)' % (search_result['snippet']['title'], search_result['id']['playlistId']))
+            
+    print('Videos:\n', '\n'.join(videos), '\n')
+    print('Channels:\n', '\n'.join(channels), '\n')
+    print('Playlists:\n', '\n'.join(playlists), '\n')
+    
+    if __name__ == '__main__':
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--q', help='Search term', default='Google')
+        parser.add_argument('--max-results', help='Max results', default=25)
+        args = parser.parse_args()
+        
+        try:
+            youtube_search(args)
+        except HttpError as e:
+            print('An HTTP error %d occurred:\n%s' % (e.resp.status, e.content))
+    
+            
     
